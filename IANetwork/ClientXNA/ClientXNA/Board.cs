51ef227dfb30b36f7e2b3cb694055a5828601db2
calimeraw <calimeraw@7Nains>
2011-10-16 17:11:53 +0200

    Ajout de square/board pour le client et d'un png pour les tiles.

---
+++ IANetwork/ClientXNA/ClientXNA/Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientXNA
+{
+    public class Board
+    {
+        #region Attributes
+        private int size_;
+        private Square[][] data_;
+        #endregion
+
+        public Board(int size)
+        {
+            size_ = size;
+            data_ = new Square[size][];
+            for (int i = 0; i < size; i++)
+                data_[i] = new Square[size];
+        }
+
+        public int Size
+        {
+            get { return size_; }
+            set
+            {
+                if (size_ != value)
+                {
+                    size_ = value;
+                    resetBoard(size_);
+                }
+            }
+        }
+
+        public Square[][] Data
+        {
+            get { return data_; }
+            private set {}
+        }
+
+
+        #region Methods
+        private void resetBoard(int size)
+        {
+            data_ = new Square[size][];
+            for (int i = 0; i < size; i++)
+                data_[i] = new Square[size];
+        }
+        #endregion
+
+    }
+}
