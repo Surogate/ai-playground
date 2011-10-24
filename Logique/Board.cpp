10a95ac10f3ab382d22f4bea1e202c51e3c29796
Errata <Errata@Errata-PC>
2011-10-24 20:24:49 +0200

    correction de l'appelle de la callback

---
+++ Logique/Board.cpp
@@ -0,0 +1,54 @@
+
+#include "Board.hpp"
+
+namespace Logique {
+
+	void Board::lock() { 
+		_mtx.lock(); 
+	}
+
+	void Board::unlock() { 
+		_mtx.unlock(); 
+	}
+
+	const Square& Board::get(const Coord& pos) const {
+		return at(pos.x)[pos.y]; 
+	}
+
+	Square& Board::get(const Coord& pos) { 
+		return at(pos.x)[pos.y]; 
+	}
+
+	const Square& Board::operator()(const Coord& pos) const { 
+		return at(pos.x)[pos.y]; 
+	}
+
+	Square& Board::operator()(const Coord& pos) { 
+		return at(pos.x)[pos.y]; 
+	}
+
+	void Board::dump() const {
+		for (unsigned int x = 0; x < BOARD_SIZE; ++x) {
+			for (unsigned int y = 0; y < BOARD_SIZE; ++y) {
+				at(x)[y].dump();
+				std::cout << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	int Board::getSquare(const Coord& pos) const {
+		if (Board::coordValid(pos)) {
+			return get(pos).getInt();
+		}
+		return Square(false).getInt();
+	}
+
+	bool Board::coordValid(const Coord& pos) {
+		return (pos.x < BOARD_SIZE && pos.y < BOARD_SIZE);
+	}
+
+	bool Board::coordValid(unsigned int x, unsigned int y) {
+		return (x < BOARD_SIZE && y < BOARD_SIZE);
+	}
+}
---
+++ Logique/Board.hpp
@@ -0,0 +1,38 @@
+
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include <boost/thread.hpp>
+#include <array>
+
+#include "Square.hpp"
+#include "Coord.hpp"
+#include "Entity.hpp"
+
+namespace Logique {
+	enum { BOARD_SIZE = 30 };
+
+	class Board : public std::array< std::array<Square, BOARD_SIZE >, BOARD_SIZE > {
+	public:
+		void lock();
+		void unlock();
+
+		const Square& get(const Coord& pos) const;
+		Square& get(const Coord& pos);
+
+		const Square& operator()(const Coord& pos) const;
+		Square& operator()(const Coord& pos);
+		
+		int getSquare(const Coord& pos) const;
+
+		void dump() const;
+
+		static bool coordValid(const Coord& pos);
+		static bool coordValid(unsigned int x, unsigned int y);
+	private:
+		boost::mutex _mtx;
+	};
+
+}
+
+#endif
