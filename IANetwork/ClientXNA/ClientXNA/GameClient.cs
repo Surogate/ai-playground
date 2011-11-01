494099fc359fab35af8843ec0305112a638d9d1c
calimeraw <calimeraw@7Nains>
2011-11-01 16:33:26 +0100

    Agrandissement de la taille de la fenetre

--- IANetwork/ClientXNA/ClientXNA/GameClient.cs
+++ IANetwork/ClientXNA/ClientXNA/GameClient.cs
@@ -49,8 +49,8 @@ namespace ClientXNA
         public GameClient()
         {
             graphics = new GraphicsDeviceManager(this);
-            this.graphics.PreferredBackBufferWidth = 600;
-            this.graphics.PreferredBackBufferHeight = 480;
+            this.graphics.PreferredBackBufferWidth = 1024;
+            this.graphics.PreferredBackBufferHeight = 720;
             this.IsMouseVisible = true;
             Content.RootDirectory = "Content";
             client_ = new TcpClient("127.0.0.1", 16000);
