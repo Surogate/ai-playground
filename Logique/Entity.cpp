03b92f60f777c0e6a04691c3bc0eb1e343f81a38
Errata <Errata@Errata-PC>
2011-11-01 19:08:42 +0100

    on divise le lot d'evaluation par 2

--- Logique/Entity.cpp
+++ Logique/Entity.cpp
@@ -165,7 +165,7 @@ namespace Logique {
 		_numberEat = 0;
 		_numberRep = 0;
 		_actual = 0;
-		_numberTot = static_cast<float>(_getSpecieNumber());
+		_numberTot = static_cast<float>(_getSpecieNumber()) / 2.f;
 	}
 
 	float Entity::computeMoy() const {
