439978288fdd9aa40ba67f2f25e83b5f9c4b8fb4
ancelf <ancelf@localhost>
2012-01-19 03:21:48 +0100

    Maj des interaction entre la Logique et le Server

--- Logique/Action.hpp
+++ Logique/Action.hpp
@@ -7,18 +7,29 @@
 namespace Logique {
 
 	struct Action {
-		typedef boost::function< void () > Functor;
+		typedef boost::function< void (unsigned int startTick) > Functor;
 
 		Action() : _tickBeforeAction(), _action() 
 		{}
 
-		Action(unsigned int time, const Functor& func) : _tickBeforeAction(time), _action(func) 
+		Action(unsigned int time_start, unsigned int time_before, const Functor& func) : _tickStart(time_start), _tickBeforeAction(time_before), _action(func) 
 		{}
 
-		void operator()() {
-			 _action();
+		bool increment(unsigned int tick_passed) {
+			_tickStart += tick_passed;
+			if (_tickStart >= _tickBeforeAction) {
+				_action(_tickStart - _tickBeforeAction);
+				return true;
+			}
+			return false;
 		}
 
+		int tickBefore() const
+		{
+			return _tickStart - _tickBeforeAction;
+		}
+
+		unsigned int _tickStart;
 		unsigned int _tickBeforeAction;
 		Functor _action;
 	};
