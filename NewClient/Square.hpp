9bd836e9cef4a00e340f1283e3343b7fc4209447
cedric.creusot <cedric.creusot@localhost>
2012-01-23 15:01:02 +0100

    Ajout du nouveau client en sfml c++

---
+++ NewClient/Square.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Square.hpp
+ * Author: Cedric Creusot <cedric.creusot@gmail.com>
+ *
+ * Created on January 18, 2012, 6:53 PM
+ */
+
+#ifndef SQUARE_HPP
+#define	SQUARE_HPP
+
+struct Square {
+public:
+    Square();
+    virtual ~Square();
+
+    int         odour;
+    bool        hasHerbs;
+};
+
+#endif	/* SQUARE_HPP */
+
